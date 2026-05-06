8cf69b2abf019744fea92e3e4f754fd5f080627a
Sabarish Kumar <sabarishsri03@gmail.com>
2026-05-06 20:38:22 +0530

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <h1>Hello React 🚀</h1>
-      <p>My first React app is working!</p>
+      <h1>Count: {count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
