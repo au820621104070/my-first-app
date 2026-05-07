b93a3d5a2da9e90aff2a2b8e9317318b11f67ffd
sabarish kumar <sabarishsri03@gmail.com>
2026-05-07 14:11:07 +0530

    Improved API product fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,38 @@ function Home(props) {
   const [products, setProducts] =
     useState([])
 
+  const [loading, setLoading] =
+    useState(true)
+
+  const [error, setError] =
+    useState("")
+
   const [searchTerm, setSearchTerm] =
     useState("")
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Failed to fetch products"
+          )
+        }
+
+        return response.json()
+      })
+
+      .then((data) => {
+        setProducts(data)
+
+        setLoading(false)
+      })
+
+      .catch((error) => {
+        setError(error.message)
+
+        setLoading(false)
+      })
   }, [])
 
   const filteredProducts = products.filter(
@@ -42,6 +67,18 @@ function Home(props) {
           }
         />
       </div>
+
+      {loading && (
+        <h2 className="message">
+          Loading products...
+        </h2>
+      )}
+
+      {error && (
+        <h2 className="message error">
+          {error}
+        </h2>
+      )}
 
       <div className="products">
         {filteredProducts.map((product) => (
